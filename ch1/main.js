42baf2e53d19ce7b482b56a578c424b19656eb57
한지원 <jiwhan@29cm.co.kr>
2022-07-07 01:44:51 +0900

    ch1 base code

---
+++ ch1/main.js
@@ -0,0 +1,9 @@
+import { readJSON } from "../fileController.js";
+import statement from "./statement.js";
+
+const invoices = readJSON("ch1/invoices.json");
+const plays = readJSON("ch1/plays.json");
+
+invoices.forEach((invoice) => {
+  console.log(statement(invoice, plays));
+});
