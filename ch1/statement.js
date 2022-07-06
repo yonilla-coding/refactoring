efe326ed914f612f2af01ee69de18d3afd583868
한지원 <jiwhan@29cm.co.kr>
2022-07-07 02:14:03 +0900

    refactor: totalAmount 변수 제거하기

--- ch1/statement.js
+++ ch1/statement.js
@@ -1,6 +1,6 @@
 const statement = (invoice, plays) => {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
   const format = (aNumber) => {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
@@ -57,14 +57,23 @@ const statement = (invoice, plays) => {
     return volumeCredits;
   };
 
+  const totalAmount = () => {
+    let result = 0;
+
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+
+    return result;
+  };
+
   for (let perf of invoice.performances) {
     result += `  ${playFor(perf).name}: ${format(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${format(totalAmount)}\n`;
+  result += `총액: ${format(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 };
