b0bdd069557f86bbc42d78f69964630313a34740
한지원 <jiwhan@29cm.co.kr>
2022-07-07 02:52:48 +0900

    refactor: htmlStatement 추가

--- ch1/statement.js
+++ ch1/statement.js
@@ -1,19 +1,15 @@
 import createStatementData from "./createStatementData.js";
 
-const statement = (invoice, plays) => {
-  return renderPlainText(createStatementData(invoice, plays));
+const usd = (aNumber) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 };
 
 const renderPlainText = (data) => {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-  const usd = (aNumber) => {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  };
 
   for (let perf of data.performances) {
     result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
@@ -22,6 +18,29 @@ const renderPlainText = (data) => {
   result += `총액: ${usd(data.totalAmount)}\n`;
   result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
+};
+
+const renderHtml = (data) => {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${usd(perf.amount)}</td>`;
+    result += `<td>${perf.audience}</td></tr>\n`;
+  }
+
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+};
+
+const statement = (invoice, plays) => {
+  return renderPlainText(createStatementData(invoice, plays));
+};
+
+const htmlStatement = (invoice, plays) => {
+  return renderHtml(createStatementData(invoice, plays));
 };
 
 export default statement;
